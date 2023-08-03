485bbaf27668396b7d2ee4cba8e44313cfdf28ab
SergeyZ <szabolotnyi777@gmail.com>
2023-08-03 21:59:08 +0200

    lessons 27 03.08.2023

---
+++ src/main/java/de/telran/javapro/lesson27_03082023/task1/Main.java
@@ -0,0 +1,29 @@
+package de.telran.javapro.lesson27_03082023.task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Два базовых способа создания отдельного потока:
+        // 1. Использование класса Thread
+        // 2. Использование интерфейса Runnable
+
+        MyThread1 myThread1 = new MyThread1();
+        MyThread2 myThread2 = new MyThread2();
+        Thread thread = new Thread(myThread2);
+
+        myThread1.test();
+
+        myThread1.start();
+        thread.start();
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Главный поток: " + i);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                System.out.println("Главный поток прерван.");
+            }
+        }
+    }
+}
---
+++ src/main/java/de/telran/practice/lesson27_03082023/FibThread.java
@@ -0,0 +1,26 @@
+package de.telran.practice.lesson27_03082023;
+
+// поток-демон раз в 5 секунд считает следующее число фиббоначи и выводит его на экран
+public class FibThread extends Thread {
+    private int prev = 1;
+    private int cur = 1;
+
+    public FibThread() {
+        setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            int temp = cur;
+            cur = prev + cur;
+            prev = temp;
+            System.out.println(cur);
+            try {
+                Thread.sleep(4500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
